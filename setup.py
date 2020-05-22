0483cf72718d3bc38d82b3f2af10a1578deb4878
Antoine Bertin <ant.bertin@gmail.com>
2020-05-23 01:45:50 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pre-commit-hooks-poetry",
-    version="0.4.0",
+    version="0.5.0",
     install_requires=["poetry"],
     packages=find_packages(),
     entry_points={"console_scripts": ["poetry-export = pre_commit_hooks.export:main"]},
